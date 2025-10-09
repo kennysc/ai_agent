60fd1377c8f50c1702ebe058da1ee8c7e086c18e
kennysc <kenny.stcyr@outlook.com>
2025-10-09 10:28:20 -0400

    move the available function out of prompt.py and into it's own file, call_function.py

--- prompts.py
+++ prompts.py
@@ -1,6 +1,3 @@
-from google.genai import types
-from functions.get_files_info import schema_get_files_info
-
 system_prompt = """
 You are a helpful AI coding agent.
 
@@ -10,8 +7,3 @@ When a user asks a question or makes a request, make a function call plan. You c
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
-available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-    ]
-)
